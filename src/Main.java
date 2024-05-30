1f2ce93d4b75c2f713e56a018b5c29d5f0ccf624
Pr1nkos <prinkos1@gmail.com>
2024-05-30 19:23:30 +0300

    tab -> 4 spaces

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,29 @@ import java.util.stream.IntStream;
 
 
 public class Main {
-	private static Random rand = new Random();
+    private static Random rand = new Random();
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int numbersToGenerate = 10000;
+        int numbersToGenerate = 10000;
 
-		List<Integer> listOfGeneratedNumbers = IntStream.range(0, numbersToGenerate)
-				.mapToObj(i -> rand.nextInt(10000))
-				.distinct()
-				.toList();
-		System.out.println("Сгенерированный лист: " + listOfGeneratedNumbers);
+        List<Integer> listOfGeneratedNumbers = IntStream.range(0, numbersToGenerate)
+                .mapToObj(i -> rand.nextInt(10000))
+                .distinct()
+                .toList();
+        System.out.println("Сгенерированный лист: " + listOfGeneratedNumbers);
 
-		//Методы сортировки
+        //Методы сортировки
 
-		Result<List<Integer>> resultSelectionSort = SortMethods.measureTime(() -> SortMethods.sortSelection(new ArrayList<>(listOfGeneratedNumbers)));
-		Result<List<Integer>> resultQuicksort = SortMethods.measureTime(() -> SortMethods.quickSort(new ArrayList<>(listOfGeneratedNumbers)));
-		System.out.println("Сортированный лист : " + resultSelectionSort.getValue());
+        Result<List<Integer>> resultSelectionSort = SortMethods.measureTime(() -> SortMethods.sortSelection(new ArrayList<>(listOfGeneratedNumbers)));
+        Result<List<Integer>> resultQuicksort = SortMethods.measureTime(() -> SortMethods.quickSort(new ArrayList<>(listOfGeneratedNumbers)));
+        System.out.println("Сортированный лист : " + resultSelectionSort.getValue());
 
-		//Замеры времени
-		System.out.println("Сортировка выбором: " + resultSelectionSort.getTime() + " миллисекунд");
-		System.out.println("Быстрая сортировка: " + resultQuicksort.getTime() + " миллисекунд");
+        //Замеры времени
+        System.out.println("Сортировка выбором: " + resultSelectionSort.getTime() + " миллисекунд");
+        System.out.println("Быстрая сортировка: " + resultQuicksort.getTime() + " миллисекунд");
 
 
 
-	}
+    }
 }
--- src/utils/SortMethods.java
+++ src/utils/SortMethods.java
@@ -11,84 +11,84 @@ import java.util.concurrent.TimeUnit;
 
 public class SortMethods {
 
-	private static int findSmallest(List<Integer> arr) {
-		int smallest = arr.getFirst();
-		int smallestIndex = 0;
-		for (int i = 1; i < arr.size(); i++) {
-			if (arr.get(i) < smallest) {
-				smallest = arr.get(i);
-				smallestIndex = i;
-			}
-		}
-		return arr.get(smallestIndex);
-	}
+    private static int findSmallest(List<Integer> arr) {
+        int smallest = arr.getFirst();
+        int smallestIndex = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) < smallest) {
+                smallest = arr.get(i);
+                smallestIndex = i;
+            }
+        }
+        return arr.get(smallestIndex);
+    }
 
-	public static List<Integer> sortSelection(List<Integer> arr) {
-		List<Integer> sorted = new ArrayList<>();
-		List<Integer> copy = new ArrayList<>(arr);
-		while (!copy.isEmpty()) {
-			int smallestNumber = findSmallest(copy);
-			sorted.add(smallestNumber);
-			copy.remove((Integer) smallestNumber);
-		}
-		return sorted;
-	}
+    public static List<Integer> sortSelection(List<Integer> arr) {
+        List<Integer> sorted = new ArrayList<>();
+        List<Integer> copy = new ArrayList<>(arr);
+        while (!copy.isEmpty()) {
+            int smallestNumber = findSmallest(copy);
+            sorted.add(smallestNumber);
+            copy.remove((Integer) smallestNumber);
+        }
+        return sorted;
+    }
 
-	public static List<Integer> quickSort(List<Integer> arr) {
+    public static List<Integer> quickSort(List<Integer> arr) {
 
-		if (arr.size() <= 1) {
-			return arr;
-		} else {
-			int pivotIndex = arr.size() / 2;
-			int pivot = arr.get(pivotIndex);
-			List<Integer> left = arr.stream()
-					.skip(1)
-					.filter(e -> e < pivot)
-					.toList();
-			List<Integer> right = arr.stream()
-					.skip(1)
-					.filter(e -> e >= pivot)
-					.toList();
-
-
-			List<Integer> sortedLeft = quickSort(left);
-			List<Integer> sortedRight = quickSort(right);
-
-			List<Integer> result = new ArrayList<>(sortedLeft);
-			result.add(pivot);
-			result.addAll(sortedRight);
-			return result;
-		}
-	}
+        if (arr.size() <= 1) {
+            return arr;
+        } else {
+            int pivotIndex = arr.size() / 2;
+            int pivot = arr.get(pivotIndex);
+            List<Integer> left = arr.stream()
+                    .skip(1)
+                    .filter(e -> e < pivot)
+                    .toList();
+            List<Integer> right = arr.stream()
+                    .skip(1)
+                    .filter(e -> e >= pivot)
+                    .toList();
 
 
+            List<Integer> sortedLeft = quickSort(left);
+            List<Integer> sortedRight = quickSort(right);
 
-	public static <T> Result<T> measureTime(Callable<T> task) {
-		long startTime;
-		T result;
-		long endTime;
-		try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-			startTime = System.currentTimeMillis();
-			Future<T> future = executor.submit(task);
+            List<Integer> result = new ArrayList<>(sortedLeft);
+            result.add(pivot);
+            result.addAll(sortedRight);
+            return result;
+        }
+    }
 
-			result = null;
 
-			try {
-				result = future.get();
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace();
-			}
 
-			endTime = System.currentTimeMillis();
-			executor.shutdown();
-			try {
-				if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-					executor.shutdownNow();
-				}
-			} catch (InterruptedException e) {
-				executor.shutdownNow();
-			}
-		}
-		return new Result<>(result, endTime - startTime);
-	}
+    public static <T> Result<T> measureTime(Callable<T> task) {
+        long startTime;
+        T result;
+        long endTime;
+        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
+            startTime = System.currentTimeMillis();
+            Future<T> future = executor.submit(task);
+
+            result = null;
+
+            try {
+                result = future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            endTime = System.currentTimeMillis();
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+            }
+        }
+        return new Result<>(result, endTime - startTime);
+    }
 }
