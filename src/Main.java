f2cd00074d57ca27955300906d80770c1a25c1d8
Pr1nkos <prinkos1@gmail.com>
2024-05-30 16:38:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import utils.Result;
+import utils.SortMethods;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+
+public class Main {
+	private static Random rand = new Random();
+
+	public static void main(String[] args) {
+
+		int numbersToGenerate = 10000;
+
+		List<Integer> listOfGeneratedNumbers = IntStream.range(0, numbersToGenerate)
+				.mapToObj(i -> rand.nextInt(10000))
+				.distinct()
+				.toList();
+		System.out.println("Сгенерированный лист: " + listOfGeneratedNumbers);
+
+		//Методы сортировки
+
+		Result<List<Integer>> resultSelectionSort = SortMethods.measureTime(() -> SortMethods.sortSelection(new ArrayList<>(listOfGeneratedNumbers)));
+		Result<List<Integer>> resultQuicksort = SortMethods.measureTime(() -> SortMethods.quickSort(new ArrayList<>(listOfGeneratedNumbers)));
+		System.out.println("Сортированный лист : " + resultSelectionSort.getValue());
+
+		//Замеры времени
+		System.out.println("Сортировка выбором: " + resultSelectionSort.getTime() + " миллисекунд");
+		System.out.println("Быстрая сортировка: " + resultQuicksort.getTime() + " миллисекунд");
+
+
+
+	}
+}
---
+++ src/utils/Result.java
@@ -0,0 +1,4 @@
+package utils;
+
+public record Result<T>(T getValue, long getTime) {
+}
